797773245395ccc9cebef58c6ad934402036d492
王宁的mac <wangning@menamobile.net>
2016-12-03 14:32:59 +0800

    Initial Commit

---
+++ YourStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YourStory
+//
+//  Created by 王宁 on 2016/12/3.
+//  Copyright © 2016年 王宁的mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
